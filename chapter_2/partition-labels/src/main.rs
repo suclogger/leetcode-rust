1d3ecd4193909ddfba219fe9ec899dff8a64829c
pr0found <runqiang.crq@alibaba-inc.com>
2021-01-18 14:16:07 +0800

    贪心-区间-划分字母区间

--- chapter_2/partition-labels/src/main.rs
+++ chapter_2/partition-labels/src/main.rs
@@ -1,38 +1,33 @@
-
 fn main() {
     let s = String::from("ababcbacadefegdehijhklij");
     assert_eq!(partition_labels(s), vec![9,7,8])
 
 }
 
-struct Pre {
-    first_occur: usize,
-    last_occur: usize,
-}
-
 pub fn partition_labels(s: String) -> Vec<i32> {
+    if s.len() < 1 {
+        return vec![]
+    }
     let mut m = std::collections::HashMap::new();
     for char_index in s.char_indices() {
-        let mut pre = m.entry(char_index.1).or_insert(Pre{
-            first_occur: char_index.0,
-            last_occur: 0,
-        });
-        pre.last_occur+=char_index.0;
+        let last_idx = m.entry(char_index.1).or_insert(char_index.0);
+        *last_idx = char_index.0;
     }
-
-    let mut res : Vec<i32> = Vec::new();
-    let mut ranges:Vec<&Pre> = m.values().collect();
-    ranges.sort_by(|a,b|a.first_occur.cmp(&b.first_occur));
-    let mut pre_l = ranges[0].first_occur;
-    let mut pre_r = ranges[0].last_occur;
-
-    for range in ranges {
-        if range.first_occur > pre_r {
-            res.push((pre_r - pre_l + 1) as i32);
-            pre_l = range.first_occur;
-            pre_l = range.last_occur;
-        } else {
-            pre_r = std::cmp::max(range.last_occur, pre_r);
+    let mut res = Vec::new();
+    let mut cur_idx = 0;
+    let mut next_idx = 0;
+    for char_index in s.char_indices() {
+        match m.get(&char_index.1) {
+            Some(last_idx) => {
+                if &char_index.0 >= &next_idx && char_index.0 >= *last_idx {
+                    res.push((char_index.0 - cur_idx + 1) as i32);
+                    cur_idx = char_index.0 + 1;
+                    next_idx = char_index.0 + 1;
+                } else {
+                    next_idx = std::cmp::max(*last_idx, next_idx);
+                }
+            },
+            None => { }
         }
     }
     res
