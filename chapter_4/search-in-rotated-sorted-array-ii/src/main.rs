ed62b52c2304480b10bd652f6d5462d0f1d2e3f7
pr0found <runqiang.crq@alibaba-inc.com>
2021-01-27 20:27:42 +0800

    做个优化

--- chapter_4/search-in-rotated-sorted-array-ii/src/main.rs
+++ chapter_4/search-in-rotated-sorted-array-ii/src/main.rs
@@ -3,31 +3,37 @@ fn main() {
     assert!(search(v, 0));
     let v = vec![2,5,6,0,0,1,2];
     assert!(!search(v, 3));
+    let v = vec![1,0,1,1,1];
+    assert!(search(v, 0));
+    let v = vec![5,1,3];
+    assert!(search(v, 3));
 }
 
 pub fn search(nums: Vec<i32>, target: i32) -> bool {
-    if nums.len() == 0 {
+    if nums.len() < 1 {
         return false;
     }
-    if nums.len() == 1 {
-        return nums[0] == target;
-    }
-    let mut l : i32 = 0;
-    let mut r: i32 = nums.len() as i32;
-    if nums[0] >= nums[nums.len() -1] {
-        let mid : usize = (l + (r - l) /2) as usize;
-        // Leetcode上需要通过Self::指向内部函数
-        // return Self::search(nums[0..mid].to_vec(), target) || Self::search(nums[mid..nums.len()].to_vec(), target);
-        return search(nums[0..mid].to_vec(), target) || search(nums[mid..nums.len()].to_vec(), target);
-    }
-    while l < r {
-        let mid = l + (r - l) /2;
-        if nums[mid as usize] == target {
+    let mut l = 0;
+    let mut r = nums.len() - 1;
+    while l <= r {
+        let mid = l + (r - l)/2;
+        if nums[mid] == target {
             return true;
-        } else if nums[mid as usize] > target {
-            r = mid;
+        }
+        if nums[l] == nums[mid] {
+            l+=1;
+        } else if nums[mid] <= nums[r] {
+            if target > nums[mid] && target <= nums[r] {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
         } else {
-            l = mid + 1;
+            if target < nums[mid] && target >= nums[l] {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
         }
     }
     false
