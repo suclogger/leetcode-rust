528ba1db00d0d411bffb3c007e0f9a2baa02e4e2
pr0found <runqiang.crq@alibaba-inc.com>
2021-07-08 17:43:49 +0800

    完全背包问题1

--- chapter_7/coin-change/src/main.rs
+++ chapter_7/coin-change/src/main.rs
@@ -1,33 +1,30 @@
-use std::cmp::{max, min};
-
 fn main() {
-    println!("Hello, world!");
+    assert_eq!(coin_change(vec![1, 2, 5], 11), 3);
+    assert_eq!(coin_change(vec![2], 3), -1);
+    assert_eq!(coin_change(vec![1], 0), 0);
+    assert_eq!(coin_change(vec![1], 1), 1);
+    assert_eq!(coin_change(vec![1], 2), 2);
+    assert_eq!(coin_change(vec![1,2147483647], 2), 2);
 }
 /**
 因为硬币的数量不限，所以是典型的DP完全背包问题：
-集合中元素可选可不选，有最小代价约束（最大金额C），求最优解
-定义dp数组 dp[i][m] 表示在前m个元素中，凑成m需要的最少硬币个数
-dp[i][m] = max(dp[i-1][m], dp[i-1][m-coin[i]] + 1)
+集合中元素可选多个（或不选），有最小代价约束（最大金额C），求最优解
+先遍历容量，再遍历物品。突破口是最后一件物品，不在乎物品一共用过几次：dp[c] = max{dp[c - cost[i]] + val[i]} for i in ...
+dp[amount] = min(dp[amount - coins[i]] + 1) for i in 0..coins.len()
+
 **/
 pub fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
-    let mut ans = coins.len() + 1;
-    let mut dp = vec![vec![coins.len() + 1; max_sum + 1]; coins.len() + 1];
-    for i in 1..=coins.len() {
-        for s in 0..=max_sum {
-            let cur_v = coins[i] as usize;
-            if s < cur_v as usize {
-                dp[i][s] = 1;
+    let mut dp = vec![i32::max_value() - 1; amount as usize+ 1];
+    dp[0] = 0;
+    for c in 1..=amount as usize {
+        for i in 0..coins.len() {
+            let cur = coins[i] as usize;
+            if c < cur {
+                dp[c] = std::cmp::min(i32::max_value() - 1, dp[c]);
             } else {
-                dp[i][s] = min(dp[i-1][s], dp[i-1][s-cur_v] + 1);
-            }
-            if s >= amount as usize {
-                ans = min(ans, dp[i][s]);
+                dp[c] = std::cmp::min(dp[c - cur] +1, dp[c]);
             }
         }
-        if ans == coins.len() + 1 { -1 } else { ans }
     }
-
-
-
-    0
+    return if dp[amount as usize] == i32::max_value() - 1 { -1 } else { dp[amount as usize] }
 }
