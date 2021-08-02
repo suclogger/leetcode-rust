7a8058b7434605a53316aa63be22ad85f9b086cd
pr0found <runqiang.crq@alibaba-inc.com>
2021-08-02 13:45:50 +0800

    周赛补充

--- week/week252/maximum-number-of-weeks-for-which-you-can-work/src/main.rs
+++ week/week252/maximum-number-of-weeks-for-which-you-can-work/src/main.rs
@@ -2,23 +2,26 @@ fn main() {
     assert_eq!(number_of_weeks(vec![1,2,3]), 6);
     assert_eq!(number_of_weeks(vec![5,2,1]), 7);
 }
+/**
+不同于别的分配任务类的题目，这题要求的返回是总数即可，按照正常的思路用priority_queue实际提交的时候会TLE
+其实就考虑所有任务中耗时最多的那个，有两种情况，一种是没超过半数，一种是超过了半数
+如果没超过半数，说明可以全部岔开，任务都可以完成
+如果超过了半数，(n/2 - (x - n/2)) * 2 = (n - x) * 2
+OOOOOOOOO OOO|OOO
+XXXXYYYZZ    |
 
+**/
 
 pub fn number_of_weeks(milestones: Vec<i32>) -> i64 {
-    let mut ans = 0i64;
-    // heap for current max work
-    let mut heap = std::collections::BinaryHeap::new();
+    let mut max: i64 = 0;
+    let mut sum: i64 = 0;
     for ms in milestones {
-        heap.push(ms);
+        max = std::cmp::max(max, ms as i64);
+        sum = sum + (ms as i64);
     }
-    let mut pre = 0;
-    while !heap.is_empty() {
-        let cur = heap.pop().unwrap();
-        if pre > 0 {
-            heap.push(pre);
-        }
-        ans = ans + 1;
-        pre = cur - 1;
+    return if max <= sum / 2 {
+        sum as i64
+    } else {
+        (sum - max ) * 2 + 1
     }
-    ans
 }
