8c426ffeb87effff07f8efad3316a1e5238fcb75
pr0found <runqiang.crq@alibaba-inc.com>
2021-08-09 17:01:58 +0800

    string 0

--- chapter_12/valid-anagram/src/main.rs
+++ chapter_12/valid-anagram/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
     println!("Hello, world!");
 }
+
+pub fn is_anagram(s: String, t: String) -> bool {
+    if s.len() != t.len() {
+        return false;
+    }
+    let mut s_table = std::collections::HashMap::new();
+    let mut t_table = std::collections::HashMap::new();
+    for s_char in s.chars() {
+        *s_table.entry(s_char).or_insert(0) += 1;
+    }
+    for t_char in t.chars() {
+        *t_table.entry(t_char).or_insert(0) += 1;
+    }
+    for (&k, &v) in s_table.iter() {
+        if let Some(&val) =  t_table.get(&k) {
+            if val != v {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+    true
+}
