10a981ed25766c96a1e313f1e1216d3056455b39
pr0found <runqiang.crq@alibaba-inc.com>
2021-09-16 23:58:33 +0800

    8行代码写了一个半小时，rust诚不我欺

--- chapter_13/reverse-linked-list/src/main.rs
+++ chapter_13/reverse-linked-list/src/main.rs
@@ -1,27 +1,31 @@
+use std::ops::{Deref, DerefMut};
+
 fn main() {
     println!("Hello, world!");
 }
-
 // Definition for singly-linked list.
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
-  pub val: i32,
-  pub next: Option<Box<ListNode>>
+    pub val: i32,
+    pub next: Option<Box<ListNode>>
 }
 
 impl ListNode {
-  #[inline]
-  fn new(val: i32) -> Self {
-    ListNode {
-      next: None,
-      val
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode {
+            next: None,
+            val
+        }
     }
-  }
 }
-// pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-//     let mut pre = None;
-//     while let Some(head) = head {
-//
-//     }
-//
-// }
+pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    let mut head = head;
+    let mut pre = Option::None;
+    while let Some(mut cur) = head {
+        head = (*cur).next;
+        (*cur).next = pre;
+        pre = Option::Some(cur);
+    }
+    pre
+}
